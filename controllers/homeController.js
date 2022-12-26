aaca791fcddf3a0fe9087d6cdcb9c3f36a04628a
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-12-26 18:26:57 +0530

    Dynamic Data added.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,11 @@
 class homeController {
-    static index(req, res) {
-        res.render('index');
+    static async index(req, res) {
+        const response = await fetchData();
+        res.render('index', { response: response.data.regional });
     }
 }
-
+function fetchData() {
+    const url = 'https://api.rootnet.in/covid19-in/stats/latest';
+    return fetch(url).then((res) => res.json());
+}
 module.exports = { homeController };
